9818561b566cc83859a68f3698395df36d3a88fe
hummusbird <birb@birb.cc>
2023-08-04 16:12:34 +0100

    init

---
+++ advent_1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace advent_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] stringelves = System.IO.File.ReadAllText("input.txt").Split("\n\n");
+        Dictionary<int, int> elves = new();
+
+        for (int i = 0; i < stringelves.Count(); i++)
+        {
+            int calories = 0;
+            foreach (string line in stringelves[i].Split("\n"))
+            {
+                calories += Int32.Parse(line);
+            }
+
+            elves.Add(i, calories);
+        }
+
+        foreach (KeyValuePair<int, int> elf in elves.OrderBy(key => key.Value))
+        {
+            Console.WriteLine(elf.Key + " - " + elf.Value);
+        }
+
+        Console.WriteLine(elves[188] + elves[84] + elves[185]);
+    }
+}
---
+++ advent_2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace advent_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] game = System.IO.File.ReadAllLines("test.txt");
+
+        // A / X = ROCK    (1)
+        // B / Y = PAPER   (2)
+        // C / Z = SCISSOR (3)
+
+        int P1_score = 0;
+        int P2_score = 0;
+
+        foreach (string round in game)
+        {
+            string P1_choice = round.Split(" ")[0];
+            string P2_choice = ConvertToABC(round.Split(" ")[1]);
+
+            if (P1_choice == P2_choice) // DRAW 
+            {
+
+            }
+        }
+    }
+
+    static string ConvertToABC(string input)
+    {
+        if (input == "X") { return "A"; }
+        if (input == "Y") { return "B"; }
+        if (input == "Z") { return "C"; }
+        return input;
+    }
+
+    static int ConvertToScore(string input)
+    {
+        if (input == "X") { return 1; }
+        if (input == "Y") { return 2; }
+        if (input == "Z") { return 3; }
+        return 0;
+    }
+}
