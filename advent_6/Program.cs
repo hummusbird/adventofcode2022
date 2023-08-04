a616ef422994425bcaf8caadeacc7700b4024b2a
hummusbird <birb@birb.cc>
2023-08-04 22:37:33 +0100

    advent_6

---
+++ advent_6/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace advent_6;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = System.IO.File.ReadAllText("input.txt");
+        Console.WriteLine(FindStart(input));
+    }
+
+    static int FindStart(string input)
+    {
+        int LENGTH_OF_MARKER = 14; // set the length of marker you're looking for here
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            string sub = input.Substring(i, LENGTH_OF_MARKER);
+
+            bool not_unique = false;
+
+            foreach (char x in sub)
+            {
+                string temp = sub.Replace(char.ToString(x), string.Empty); // remove character once
+
+                if (temp.Length <= LENGTH_OF_MARKER - 2) // if two characters removed, the string is not unique
+                {
+                    not_unique = true;
+                }
+            }
+
+            if (not_unique == false) { return i + LENGTH_OF_MARKER; } // if the string has not had two characters removed ever, it IS unique
+        }
+        return -1;
+    }
+}
