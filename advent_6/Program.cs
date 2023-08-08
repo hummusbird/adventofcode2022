6300b24262091283b05ae16f30aeac4ce28622bd
hummusbird <birb@birb.cc>
2023-08-08 09:53:30 +0100

    significantly improve advent_6

--- advent_6/Program.cs
+++ advent_6/Program.cs
@@ -3,32 +3,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        string input = System.IO.File.ReadAllText("input.txt");
-        Console.WriteLine(FindStart(input));
-    }
-
-    static int FindStart(string input)
-    {
         int LENGTH_OF_MARKER = 14; // set the length of marker you're looking for here
+        string input = File.ReadAllText("input.txt");
 
         for (int i = 0; i < input.Length; i++)
         {
             string sub = input.Substring(i, LENGTH_OF_MARKER);
-
-            bool not_unique = false;
-
-            foreach (char x in sub)
-            {
-                string temp = sub.Replace(char.ToString(x), string.Empty); // remove character once
-
-                if (temp.Length <= LENGTH_OF_MARKER - 2) // if two characters removed, the string is not unique
-                {
-                    not_unique = true;
-                }
-            }
-
-            if (not_unique == false) { return i + LENGTH_OF_MARKER; } // if the string has not had two characters removed ever, it IS unique
+            string distinct = new(sub.Distinct().ToArray());
+            // if the string has any duplicates, they will be removed and the length of string will change.
+            if (distinct.Length == sub.Length) { Console.WriteLine(i + LENGTH_OF_MARKER); Environment.Exit(1); }
         }
-        return -1;
     }
 }
